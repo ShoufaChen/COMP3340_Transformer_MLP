7262ef93fb85165cc1e7331eeed3707048ec172e
ShoufaChen <shoufachen@163.com>
2022-01-31 08:04:39 +0800

    update config

--- configs/_base_/models/vit_base_flowers.py
+++ configs/_base_/models/vit_base_flowers.py
@@ -2,27 +2,24 @@
 model = dict(
     type='ImageClassifier',
     backbone=dict(
-        return_tuple=False,
         type='VisionTransformer',
-        num_layers=12,
-        embed_dim=768,  #
-        num_heads=12,
+        arch='b',
         img_size=224,
         patch_size=16,
-        in_channels=3,
-        feedforward_channels=3072,
         drop_rate=0.1,
-        attn_drop_rate=0.),
+        init_cfg=[
+            dict(
+                type='Kaiming',
+                layer='Conv2d',
+                mode='fan_in',
+                nonlinearity='linear')
+        ]),
     neck=None,
     head=dict(
         type='VisionTransformerClsHead',
-        num_classes=17,  # modify
+        num_classes=17,
         in_channels=768,
-        hidden_dim=3072,
-        loss=dict(type='LabelSmoothLoss', label_smooth_val=0.1),
-        topk=(1, 5),
-    ),
-    train_cfg=dict(
-        augments=dict(type='BatchMixup', alpha=0.2, num_classes=17,  # modify
-                      prob=1.))
-)
+        loss=dict(
+            type='LabelSmoothLoss', label_smooth_val=0.1,
+            mode='classy_vision'),
+    ))
