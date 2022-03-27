d1f8c519eca92dd6af3436692820dfbe191dbaf2
EE-LiuYunhao <liuyunhao1578@gmail.com>
2022-03-27 22:40:27 +0800

    merge different resmlp configs

--- configs/flowers/resmlp_small_wda.py
+++ configs/flowers/resmlp_small_wda.py
@@ -1,6 +1,7 @@
 _base_ = [
-    '../_base_/models/resmlp_base_flowers.py',
+    '../_base_/models/resmlp_small_flowers.py',
     '../_base_/datasets/flowers_bs32.py',
     '../_base_/schedules/flowers_bs32.py',
     '../_base_/default_runtime.py'
 ]
+
