7f703790326f8d7136ee5918fe15868ca40ad927
LawManTing <71928233+LawManTing@users.noreply.github.com>
2022-03-29 20:45:15 +0800

    Update mlp_mixer_sda.py

--- configs/flowers/mlp_mixer_sda.py
+++ configs/flowers/mlp_mixer_sda.py
@@ -1,9 +1,10 @@
 _base_ = [
     '../_base_/models/mlp_mixer_base_patch16.py',
-    '../_base_/datasets/imagenet_bs64_mixer_224.py',
-    '../_base_/schedules/imagenet_bs4096_AdamW.py',
+    '../_base_/datasets/flowers_bs32.py',
+    '../_base_/schedules/flowers_bs32.py',
     '../_base_/default_runtime.py',
 ]
+
 
 policies = [
     [
