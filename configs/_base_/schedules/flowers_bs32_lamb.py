d3d3fe16b04874442622429f3ce80d2c7211402f
EE-LiuYunhao <liuyunhao1578@gmail.com>
2022-04-03 18:02:16 +0800

    add lamb schedule config for flowers

---
+++ configs/_base_/schedules/flowers_bs32_lamb.py
@@ -0,0 +1,37 @@
+# optimizer, modified from cifar10_bs128.py
+# optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+# optimizer_config = dict(grad_clip=None)
+# # learning policy
+# lr_config = dict(policy='step', step=[100, 150])
+# runner = dict(type='EpochBasedRunner', max_epochs=200)
+
+paramwise_cfg = dict(
+    norm_decay_mult=0.0,
+    bias_decay_mult=0.0,
+    custom_keys={
+        '.absolute_pos_embed': dict(decay_mult=0.0),
+        '.relative_position_bias_table': dict(decay_mult=0.0)
+    })
+
+# for batch in each gpu is 128, 8 gpu
+# lr = 5e-4 * 128 * 8 / 512 = 0.001
+optimizer = dict(
+    type='Lamb',
+    lr= 0.005,
+    weight_decay=0.2,
+    #paramwise_cfg=paramwise_cfg)
+)
+optimizer_config = dict(grad_clip=dict(max_norm=5.0))
+
+# learning policy
+lr_config = dict(
+    policy='CosineAnnealing',
+    by_epoch=False,
+    min_lr_ratio=1e-2,
+    warmup='linear',
+    warmup_ratio=1e-3,
+    warmup_iters=100,
+    warmup_by_epoch=False)
+
+runner = dict(type='EpochBasedRunner', max_epochs=100)
+
